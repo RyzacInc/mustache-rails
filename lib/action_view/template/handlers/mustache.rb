c776bd7f9e6e221104841b2c03872d117902eb19
Amjad Masad <amjad.masad@gmail.com>
2012-06-27 15:14:04 -0400

    Add translate preprocess for sending to the client

--- lib/action_view/template/handlers/mustache.rb
+++ lib/action_view/template/handlers/mustache.rb
@@ -22,7 +22,7 @@ module ActionView
         # Returns String of Ruby code to be evaled.
         def self.call(template)
           # Use standard mustache parser to generate tokens
-          tokens = ::Mustache::Parser.new.compile(template.source)
+          tokens = ::Mustache::Parser.new.compile(self.expand_t(template.source))
 
           # Use custom generator to generate the compiled ruby
           src = ActionView::Mustache::Generator.new.compile(tokens)
@@ -30,6 +30,12 @@ module ActionView
           <<-RUBY
             ctx = mustache_view.context; ctx.push(local_assigns); #{src}
           RUBY
+        end
+
+        def self.expand_t(source)
+          source.gsub(/<%t([\s\S]+?)%>/) { |match|
+            I18n.t($1.strip)
+          }
         end
       end
     end
